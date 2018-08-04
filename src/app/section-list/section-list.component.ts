36e037a9869f3f2f0b1bbdbd1c370c9d74d07086
Paarth Kotak <kotak.p@husky.neu.edu>
2018-08-04 13:37:27 -0400

    section list implementation

---
+++ src/app/section-list/section-list.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import {SectionServiceClient} from '../service/section.service.client';
+import {CourseNavigatorServiceClient} from '../service/course-navigator.service.client';
+import {UserServiceClient} from '../service/user.service.client';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-sections',
+  templateUrl: './section-list.component.html',
+  styleUrls: ['./section-list.component.css']
+})
+export class SectionListComponent implements OnInit {
+
+  courses = [];
+  sections = [];
+  selectedCourse = {
+    id: -1,
+    title: ''
+  };
+  section = {name: '', seats: ''};
+  toEnroll = true;
+  constructor(private sectionService: SectionServiceClient,
+              private courseService: CourseNavigatorServiceClient,
+              private userService: UserServiceClient,
+              private router: Router) { }
+
+  selectCourse = course => {
+    this.selectedCourse = course;
+    this.sectionService
+      .findSectionsForCourse(course.id)
+      .then(sections => this.sections = sections);
+  }
+
+  addSection = section => {
+    section.courseId = this.selectedCourse.id;
+    this.sectionService
+      .createSection(section)
+      .then(() => {
+        return this.sectionService
+          .findSectionsForCourse(this.selectedCourse.id);
+      })
+      .then(sections => this.sections = sections);
+  }
+
+  enroll = section => {
+    this.sectionService
+      .enrollStudentInSection(section._id, this.toEnroll)
+      .then(() => {
+        alert('successfully enrolled:');
+        return this.sectionService
+          .findSectionsForCourse(this.selectedCourse.id);
+      })
+      .then(() => this.router.navigate(['/profile']));
+  }
+
+  deleteSection = section => {
+    const sectionId = section._id;
+    const courseId = section.courseId;
+    this.sectionService
+      .deleteSection(sectionId)
+      .then(() => {
+        return this.sectionService
+          .findSectionsForCourse(courseId);
+      })
+      .then(sections => this.sections = sections);
+  }
+
+  logout() {
+    this.userService
+      .logout()
+      .then(() => this.router.navigate(['home']));
+  }
+
+  ngOnInit() {
+    this.courseService.findAllCourses()
+      .then(courses => this.courses = courses);
+  }
+
+}
