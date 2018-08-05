64d5a3f5c0a646ef23c1a0e982f957eeecfc0ae3
Paarth Kotak <kotak.p@husky.neu.edu>
2018-08-05 17:50:27 -0400

    UI fixes.

--- src/app/section-list/section-list.component.ts
+++ src/app/section-list/section-list.component.ts
@@ -4,6 +4,7 @@ import {CourseNavigatorServiceClient} from '../service/course-navigator.service.
 import {UserServiceClient} from '../service/user.service.client';
 import {Router} from '@angular/router';
 import {Section} from '../models/section.model.client';
+import {User} from '../models/user.model.client';
 
 @Component({
   selector: 'app-sections',
@@ -20,6 +21,7 @@ export class SectionListComponent implements OnInit {
   };
   section: Section = new Section();
   toEnroll = true;
+  user: User = new User();
   constructor(private sectionService: SectionServiceClient,
               private courseService: CourseNavigatorServiceClient,
               private userService: UserServiceClient,
@@ -33,14 +35,19 @@ export class SectionListComponent implements OnInit {
   }
 
   enroll = section => {
-    this.sectionService
-      .enrollStudentInSection(section._id, this.toEnroll)
-      .then(() => {
-        alert('successfully enrolled');
-        return this.sectionService
-          .findSectionsForCourse(this.selectedCourse.id);
-      })
-      .then(() => this.router.navigate(['/profile']));
+    if (this.user.firstName === undefined) {
+      alert('Please log in to enroll');
+      this.router.navigate(['/login']);
+    } else {
+      this.sectionService
+        .enrollStudentInSection(section._id, this.toEnroll)
+        .then(() => {
+          alert('successfully enrolled');
+          return this.sectionService
+            .findSectionsForCourse(this.selectedCourse.id);
+        })
+        .then(() => this.router.navigate(['/profile']));
+    }
   }
 
   logout() {
@@ -50,6 +57,10 @@ export class SectionListComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.userService
+      .profile()
+      .then(user => this.user = user);
+
     this.courseService.findAllCourses()
       .then(courses => this.courses = courses);
   }
