b66a6d07cf78c1ffef159eda3b0d1121ebad8055
Paarth Kotak <kotak.p@husky.neu.edu>
2018-08-06 10:31:21 -0400

    changed app name to local app name

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cs5610-summer2-2018-angular'));
+app.use(express.static(__dirname + '/dist/webdev-summer2-angular-pkotak'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/cs5610-summer2-2018-angular/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/webdev-summer2-angular-pkotak/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
